afe03ae18628492244e3477d50d5be22ce49bdc6
vivekkumarsah2 <vivekkumarsah2@gmail.com>
2018-12-16 16:13:21 +0530

    Add files via upload

---
+++ Login Page/js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_rmbR7uIY1', // e.g. us-east-2_uXboG5pAb
+        region: 'eu-west-1', // e.g. us-east-2
+		clientId: '4pufcq8absfkveg8m0qeltea0b' //is this used anywhere?
+    },
+};
+
